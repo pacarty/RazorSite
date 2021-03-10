d43d1720be12d5545169980f245f677e0b089fd7
Patrick Carty <carty.patrick@gmail.com>
2021-03-11 06:52:54 +1100

    Added Movie class and data context

---
+++ RazorSite/Data/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace RazorSite.Data
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+    }
+}
