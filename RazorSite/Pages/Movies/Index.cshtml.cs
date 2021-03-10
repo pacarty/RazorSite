923c810e9a07c68dce862f71e77824401c9c2c4e
Patrick Carty <carty.patrick@gmail.com>
2021-03-11 07:41:32 +1100

    Create Movies Index page and model
    After manual addition of movies, this data can be seen from the movies/index page.

---
+++ RazorSite/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorSite.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorSite.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DataContext _context;
+        public IList<Movie> MovieList { get; set; }
+
+        public IndexModel(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task OnGetAsync()
+        {
+            MovieList = await _context.Movies.ToListAsync();
+        }
+    }
+}
