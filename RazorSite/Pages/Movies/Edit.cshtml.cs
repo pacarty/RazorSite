50ac9d1f86ad962d24202eec82597562e01d8246
Patrick Carty <carty.patrick@gmail.com>
2021-03-11 23:52:18 +1100

    Added edit page and model
    To get to edit page, append id?=x where x is the primary key of the selected movie. eg: https://localhost:44360/movies/edit?id=2

---
+++ RazorSite/Pages/Movies/Edit.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorSite.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorSite.Pages.Movies
+{
+    public class EditModel : PageModel
+    {
+        private readonly DataContext _context;
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public EditModel(DataContext context)
+        {
+            _context = context;
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movies.Any(e => e.MovieId == id);
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movies.FirstOrDefaultAsync(m => m.MovieId == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Movie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(Movie.MovieId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+
+}
