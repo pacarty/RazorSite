71d88f3ad6c8863897e1066bb1d9671abde4a718
Patrick Carty <carty.patrick@gmail.com>
2021-03-12 00:55:07 +1100

    Added delete page and model
    Navigation currently works the same way as edit and details (check those commit details if unsure)

---
+++ RazorSite/Pages/Movies/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorSite.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorSite.Pages.Movies
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly DataContext _context;
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public DeleteModel(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movies.FirstOrDefaultAsync(m => m.MovieId == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movies.FindAsync(id);
+
+            if (Movie != null)
+            {
+                _context.Movies.Remove(Movie);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
