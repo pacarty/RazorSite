981d67e96cd3c79793796858b8ea749416a39b18
Patrick Carty <carty.patrick@gmail.com>
2021-03-11 19:29:19 +1100

    Added Details page and model
    To view details, append id?=x where x is the primary key of the selected movie. eg: https://localhost:44360/movies/details?id=2

---
+++ RazorSite/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorSite.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorSite.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly DataContext _context;
+
+        public Movie Movie { get; set; }
+
+        public DetailsModel(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movies.FirstOrDefaultAsync(m => m.MovieId == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
