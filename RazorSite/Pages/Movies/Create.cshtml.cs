5ce4d0f4f5df9ac7d06677d79d71b3992790062a
Patrick Carty <carty.patrick@gmail.com>
2021-03-11 20:33:54 +1100

    Added Create page and model

---
+++ RazorSite/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorSite.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorSite.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DataContext _context;
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public CreateModel(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movies.Add(Movie);
+            await _context.SaveChangesAsync();
+
+
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
